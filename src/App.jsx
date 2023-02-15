da97c0f3306a569457f640bc103e527739c805ed
nikolai-chernolutskii <nikolai@site-now.online>
2023-02-15 15:01:38 +0200

    fix: tweak root div styling for proper display

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Spline from "@splinetool/react-spline";
+import "./App.css";
 
 function App() {
   return (
-    <Spline scene="https://prod.spline.design/QwBYb3Ge8ARRh5Ir/scene.splinecode" />
+    <Spline
+      className="spline_container"
+      scene="https://prod.spline.design/QwBYb3Ge8ARRh5Ir/scene.splinecode"
+    />
   );
 }
 
